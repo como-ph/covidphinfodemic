6ddaa02ce80fe06d14c04e2283934787d148f977
Ernest Guevarra <ernest@guevarra.io>
2020-07-25 14:02:33 +0100

    initial commit

---
+++ R/covidphinfodemic.R
@@ -0,0 +1,25 @@
+################################################################################
+#
+#' Utilities to Search and Extract Metadata from Philippines-Focused
+#' Infographics and Dashboards
+#'
+#' The COVID-19 pandemic can be characterised by the epidemiological
+#' phenomenon and transmission dynamics of this novel coronavirus causing not
+#' only significant global health consequences but also societal and economic
+#' impacts. However, a much less recognised yet equally significant phenomenon
+#' of COVID-19 infodemic characterised as the excessive amount of information
+#' about the pandemic, which makes the identification of a solution difficult
+#' and can spread misinformation, disinformation and rumours during the health
+#' emergency has been co-existing. The infodemic has hampered an effective
+#' public health response and hasa created confusion and distrust among people.
+#' We aim to document and analyse this phenomenon in the context of the
+#'
+#' @docType package
+#' @name covidphinfodemic
+#' @keywords internal
+#'
+#'
+#'
+#
+################################################################################
+"_PACKAGE"
